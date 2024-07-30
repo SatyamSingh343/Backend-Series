890d6290d50cddde3d5889fc4efe986201966ac1
SatyamSingh343 <satyamkr304@gmail.com>
2024-07-30 14:57:53 +0530

    new

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,48 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET ,// Click 'View Credentials' below to copy your API secret
+    });
+
+
+const uploadOnCloudinary=async(localFilePath)=>{
+    try{
+        if(!localFilePath)return null
+        //upload the file on cloudinary
+        const response =await cloudinary.uploader.upload(
+            localFilePath,
+            {
+            resource_type:"auto"
+        })
+        //file has been uploaded succesfully
+        console.log("file is uploaded on cloudinary",response.url);
+        return response
+
+
+    }
+    catch (error){
+        fs.unlinkSync(localFilePath)// remove the localy saved temporary file as the upload opertaion got failed.
+        return null;
+
+    }
+}   
+
+
+const uploadResult = await cloudinary.uploader
+.upload(
+    'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+)
+.catch((error) => {
+    console.log(error);
+
+});
+
+export {uploadOnCloudinary}
