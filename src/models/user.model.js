e0f1ee14f6b91aa6630d4df504c06b6636f36e81
SatyamSingh343 <satyamkr304@gmail.com>
2024-07-30 12:34:30 +0530

    Again new commit

---
+++ src/models/user.model.js
@@ -0,0 +1,70 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema=new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true,
+        },
+        avatar:{
+            type:String,//cloudinary service will be used here for this thing
+            required:true,
+        },
+        coverImage:{
+            type:String,//cloudinary url
+        },
+        watchHistory:{
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        },
+        password:
+        {
+            type:String,
+            required:[true,'Password is required']
+        },
+        refreshToken:{
+            type:String,
+
+        },
+
+
+
+
+},
+{
+    timestamps:true 
+}
+)
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function
+(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+
+export const Usre=mongoose.model("User",userSchema)
