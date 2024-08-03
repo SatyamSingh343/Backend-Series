25d39147d18168bc2ca251e7f0dcbe0acaee9982
SatyamSingh343 <satyamkr304@gmail.com>
2024-08-03 16:59:43 +0530

    new committed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ const registerUser=asynchandler(async(req,res)=>{
     // check for user creation 
     // return response
 
-   const {fullname,email,username,password} = req.body
+   const {fullname,email,username,password} = req.body;
    console.log("email:",email);
 
 //    if(fullname === ""){
@@ -30,23 +30,26 @@ if(
 
    }
 
-   const existedUser=User.findOne({
+   const existedUser=await User.findOne({
     $or:[{username}, {email}]
    })
    if(existedUser){
     throw new ApiError(409,"User with email or username already exists")
    }
-
+   //  console.log(req.files);
    const avatarLocalPath= req.files?.avatar[0]?.path // multer ne help kiya file store krne pe aur phir yeh function file ko store krta haia ur phir baad main file name return krta hai uske path ke saath .
-   const coverImageLocalPath =req.files?.CoverImage[0]?.path;
-
+   // const coverImageLocalPath =req.files?.coverImage[0]?.path;
+   let coverImageLocalPath;
+   if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+      coverImageLocalPath=req.files.coverImage[0].path
+   }
    if(!avatarLocalPath){
     throw new ApiError(400,"Avatar is required")
    } 
 
    const avatar = await uploadOnCloudinary(avatarLocalPath)
    const CoverImage= await uploadOnCloudinary(coverImageLocalPath)
-   
+  
    if(!avatar){
     throw new ApiError(400,"Avatar is required")
    } 
@@ -55,7 +58,7 @@ if(
    const user = await User.create({
     fullname,
     avatar:avatar.url,
-    CoverImage:CoverImage?.url || "",
+    coverImage:CoverImage?.url || "",
     email,
     password,
     username:username.toLowerCase(),
